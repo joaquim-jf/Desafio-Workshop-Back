7fd43ceb1a13b55b3eabf6ae2614d0e9a6c03246
Joaquim Felix <65045562+joaquim-jf@users.noreply.github.com>
2026-03-16 19:05:56 -0300

    Update Ata.cs

--- DesafioFastBack/Models/Ata.cs
+++ DesafioFastBack/Models/Ata.cs
@@ -1,11 +1,13 @@
 namespace DesafioFastBack.Models
 {
-   {
     public class Ata
     {
         public int Id { get; set; }
+        
+        // Use o nome da propriedade exatamente como está na sua Controller (singular)
         public Workshop workshop { get; set; } = new Workshop();
+        
+        // Use o nome da propriedade exatamente como está na sua Controller (singular)
         public List<Colaborador> Colaborador { get; set; } = new List<Colaborador>(); 
-        }
-}
+    }
 }
