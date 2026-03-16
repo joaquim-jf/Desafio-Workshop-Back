6414219c6462b06d6f298f246fd1cddb385baddd
Joaquim Felix <65045562+joaquim-jf@users.noreply.github.com>
2026-03-16 18:02:31 -0300

    Update Ata.cs

--- DesafioFastBack/Models/Ata.cs
+++ DesafioFastBack/Models/Ata.cs
@@ -1,9 +1,16 @@
-﻿namespace DesafioFastBack.Models
+namespace DesafioFastBack.Models
 {
     public class Ata
     {
         public int Id { get; set; }
-        public Workshop workshop { get; set; } = new Workshop();
-        public List<Colaborador> Colaborador { get; set; } = new List<Colaborador>(); 
+
+        // Adicione esta linha: é o ID que liga ao Workshop
+        public int WorkshopId { get; set; }
+        
+        // Mantenha a propriedade de navegação (opcionalmente como nula para o POST não exigir o objeto todo)
+        public Workshop? workshop { get; set; }
+
+        // Para os colaboradores, se for uma relação N para N, o ideal é manter a lista
+        public List<Colaborador> Colaboradores { get; set; } = new List<Colaborador>(); 
     }
 }
