deb3f18d31dca74980355393b479509a3fa9983d
Joaquim Felix <65045562+joaquim-jf@users.noreply.github.com>
2026-03-16 18:13:02 -0300

    Make workshop property nullable in Ata class

--- DesafioFastBack/Models/Ata.cs
+++ DesafioFastBack/Models/Ata.cs
@@ -4,7 +4,7 @@ namespace DesafioFastBack.Models
     public class Ata
     {
         public int Id { get; set; }
-        public Workshop workshop { get; set; } = new Workshop();
+        public Workshop? workshop { get; set; } 
         public List<Colaborador> Colaborador { get; set; } = new List<Colaborador>(); 
         }
 }
